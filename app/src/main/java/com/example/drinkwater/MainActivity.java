e8d0ddfeb552f09e247330c525c246e8d291757c
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-10-03 08:46:10 -0300

    Connect logic with layout

--- app/src/main/java/com/example/drinkwater/MainActivity.java
+++ app/src/main/java/com/example/drinkwater/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.drinkwater;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonNotify;
+    private EditText editTextMinutes;
+    private TimePicker timePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonNotify = findViewById(R.id.button_notify);
+        editTextMinutes = findViewById(R.id.edit_text_interval);
+        timePicker = findViewById(R.id.time_picker);
+
+        timePicker.setIs24HourView(true);
+
+        
     }
 }
